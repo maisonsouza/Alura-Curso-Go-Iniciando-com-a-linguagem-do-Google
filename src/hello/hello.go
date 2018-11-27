280afd72dd78147d04e08dc7a4216d55d0ea9502
Maison Galvao <maisongalvao144@gmail.com>
2018-11-27 13:59:17 -0400

    Aula02 - Trabalhando com variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, MAISON!!")
-	fmt.Println("Meu primeiro programa em Go")
-	fmt.Println("Preciso de ponto e virgula ?")
-	fmt.Println("Sim ou Não ?")
+	nome := "Maison"
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("O programa está na versão", versao)
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir os logs")
+	fmt.Println("3 - Sair")
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi",comando)
+
 }
