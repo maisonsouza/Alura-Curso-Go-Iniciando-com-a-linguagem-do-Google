3102ba144dd7296ceb570382da2377ccdcf438f4
Maison Galvao <maisongalvao144@gmail.com>
2018-11-27 11:10:15 -0400

    Aula01 - Introdução ao Go

---
+++ src/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World, MAISON!!")
+	fmt.Println("Meu primeiro programa em Go")
+	fmt.Println("Preciso de ponto e virgula ?")
+	fmt.Println("Sim ou Não ?")
+}
